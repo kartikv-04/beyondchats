b54266793bcf2701e3fed95848c628000a368a2b
Kartik Varia <kartikvaria101@gmail.com>
2025-12-28 19:36:14 +0530

    Initialized backend and added server file

---
+++ backend/src/index.js
@@ -0,0 +1,45 @@
+// Import all necessary packages
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// config dotenv
+dotenv.config();
+
+// Create express instance
+const app = express();
+
+// use all app functions
+app.use(express.json());
+
+// enable cors
+app.use(cors({
+    origin : '*',
+    methods : ["GET", "POST", "PATCH"],
+    credentials : true
+}));
+
+// Function to connect database
+async function connectDB (){
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Database Connected Successfully");
+    }
+    catch(error){
+        console.error("Error in connecting Databased : ", error);
+    }
+}
+
+// Listen to Server
+let PORT=5000;
+app.listen(PORT, ()=>{
+    connectDB();     // Connect Database here 
+    console.log(`Server Started  : http://localhost:${PORT}`);
+});
+
+// Health check route
+app.get("/",(req, res)=>{
+    res.send("API Working .....");
+})
+
